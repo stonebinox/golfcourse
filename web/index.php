7287f2e823313a69bcf4714e0a30919bf19e073f
stonebinox <anoop.santhanam@gmail.com>
2017-10-21 22:15:28 +0530

    DB Fix

--- web/index.php
+++ web/index.php
@@ -22,10 +22,10 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
 $app->register(new Silex\Provider\DoctrineServiceProvider(), array(
     'db.options' => array(
       'driver' => 'pdo_mysql',
-      'dbname' => 'heroku_bc01d6c27e92b76',
+      'dbname' => 'heroku_db2',
       'user' => 'root',
       'password' => 'adminspin#123',
-      'host'=> "localhost",
+      'host'=> "cloud.spectralinsights.com",
     )
   ));
 
